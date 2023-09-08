8a357299ac50dc85a47d85779efea343a11f51fa
Dilipr9880 <133027253+Dilipr9880@users.noreply.github.com>
2023-09-08 13:25:36 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const multer = require('multer');
+const { MONGODB_URL } = require('./config');
+const app = express();
+const { router } = require('./multer-configuration/profile-image');
+const { tweetRouter } = require('./multer-configuration/tweet-image');
+const dotenv = require('dotenv');
+dotenv.config();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+//Connecting to MONGODB
+mongoose.connect(process.env.MONGODBURL);
+
+//Checking if it is connected
+mongoose.connection.on('connected', () => console.log('DB Connected'));
+mongoose.connection.on('error', (error) => console.log(error));
+
+//inserting schemas
+require('./models/user_model');
+require('./models/tweet_model');
+
+//inserting routes
+app.use('/API/auth', require('./routes/auth'));
+app.use('/API', require('./routes/user'));
+app.use('/api/tweet', require('./routes/tweet'));
+app.use(router); //profile picture download
+app.use(tweetRouter); //tweetpicture download
+
+//testing route
+app.get('/deploy', (req, res) => {
+  return res.status(200).send(`<h1>Inserted successfully</h1>`);
+});
+
+app.listen(9000, () => console.log('Server Connected at Port 9000'));
